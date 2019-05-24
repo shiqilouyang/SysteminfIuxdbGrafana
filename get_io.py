d2b298af24d81ad078783614d69065b01f23f94f
sxu <shiqilouyang@163.com>
2019-05-24 16:25:23 +0800

    drop aep massage get

--- get_io.py
+++ get_io.py
@@ -16,14 +16,3 @@ def get_w_r():
             list_r.append(float(list_w_r[0]))
             list_w.append(float(list_w_r[1]))
     return '%.2f'%(sum(list_r)/5), '%.2f'%(sum(list_w)/5)
-
-
-
-def get_AEP_Use():
-    use_aep = []
-    for i in bylineread('df-h.log'):
-        if i.startswith("/dev/pmem"):
-            use_aep.append(str(int(re.findall(' \d+', i)[1].strip())/1024) + "G") \
-                if "M" in i \
-                else use_aep.append(re.findall(' \d+', i)[1].strip() + "G")
-    return float(use_aep[0][:-1]) + float(use_aep[1][:-1])
