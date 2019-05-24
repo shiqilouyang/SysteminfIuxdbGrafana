6c1dc56dcf118e1b15ee105bd74895af244ba04c
sxu <shiqilouyang@163.com>
2019-05-24 16:30:37 +0800

    drop aep massage get

--- cpu_memo_disk.py
+++ cpu_memo_disk.py
@@ -2,18 +2,18 @@ import time
 from abc import ABCMeta
 from six import add_metaclass
 import psutil
-from get_io import get_w_r, get_AEP_Use
+from get_io import get_w_r
 from utils.safe_linux import OSUtil as os
 from utils import record_parse_result
 from utils.multipo_queque import Multipo_Queque
-from utils.influx_used import CreateInfluxdbDatabase,CreateInfluxdbRetentionPolicy
+from utils.influx_used import CreateInfluxdbDatabase, CreateInfluxdbRetentionPolicy
 
 
 @add_metaclass(ABCMeta)
 class InfluxdbOperation(object):
     def __init__(self):
         self.q = Multipo_Queque()
-        #self.ps = psutil.virtual_memory()
+        # self.ps = psutil.virtual_memory()
         self.queque = None
         #self.list = list()
         self.measurement_cpu = "cpu"
@@ -21,8 +21,7 @@ class InfluxdbOperation(object):
         self.measurement_io_read = "io_read"
         self.measurement_io_write = "io_write"
         self.measurement_io_wait = "io_wait"
-        self.measurement_aep_use = "aep_used"
- 
+
     def get_message(self, app_label, state):
         pass
 
@@ -58,26 +57,24 @@ class GetMessage(InfluxdbOperation):
             # else:
             #     io_write = (p2_write_end-p1_write_end)/(p2_write_time-p1_write_time)
 
-            os.run_linux("iostat -d 1 5 > io_message.log", asyn=False)
-            os.run_linux("df -h > get_io.log", asyn=False)
+            os.run_linux("iostat -d 1 5 > io_message.txt", asyn=False)
             io_read, io_write = get_w_r()
             self.num += 1
-            data, NULL, NULL = self.q.run('%.2f' % (psutil.cpu_times_percent(interval=None, percpu=False).user))
+            (data, self.list, self.queque) = self.q.run(
+                '%.2f' % (psutil.cpu_times_percent(interval=None, percpu=False).user))
             record_parse_result(self.measurement_cpu, self.num, data)
-            data, NULL, NULL = self.q.run('%.2f'% (psutil.virtual_memory().used/1024/1024/1024))
+            (data, self.list, self.queque) = self.q.run('%.2f' % (psutil.virtual_memory().used / 1024 / 1024 / 1024))
             record_parse_result(self.measurement_memory, self.num, data)
-            data, NULL, NULL = self.q.run(io_read)
+            (data, self.list, self.queque) = self.q.run(io_read)
             record_parse_result(self.measurement_io_read, self.num, data)
-            data, NULL, NULL = self.q.run(io_write)
+            (data, self.list, self.queque) = self.q.run(io_write)
             record_parse_result(self.measurement_io_write, self.num, data)
-            data, NULL, NULL = self.q.run(
+            (data, self.list, self.queque) = self.q.run(
                 '%.2f' % (psutil.cpu_times_percent(interval=None, percpu=False).iowait)
             )
             record_parse_result(self.measurement_io_wait, self.num, data)
 
-            data, NULL, NULL = self.q.run(get_AEP_Use())
-            record_parse_result(self.measurement_aep_use, self.num, data)
 
 if __name__ == '__main__':
     c = GetMessage()
-    c.gen_message()
+    print(c.gen_message())
